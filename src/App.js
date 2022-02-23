4d739eb4638eff78b7e12ec2b40481b27723a0e0
mirandali17 <lily88zym@gmail.com>
2022-02-24 04:22:53 +0800

    router function

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/navbar.component"
+import ExercisesList from "./components/exercises-list.component";
+import EditExercise from "./components/edit-exercise.component";
+import CreateExercise from "./components/create-exercise.component";
+import CreateUser from "./components/create-user.component";
+
 function App() {
   return (
-    <div className="container">
-      Hello world!
-    </div>
+    <Router>
+    
+      <div className='container'>
+      
+        <Navbar />
+        <br />
+        
+        <Routes>
+          <Route path="/" element={<ExercisesList />} />
+          <Route path="/edit/:id" element={<EditExercise />} />
+          <Route path="/create" element={<CreateExercise />} />
+          <Route path="/user" element={<CreateUser />} />
+        </Routes>
+      </div>
+      </Router>
   );
 }
 
---
+++ src/components/exercises-list.component.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function ExercisesList() {
+  return (
+    <div>
+        <p>You are on the Exercises List component!</p>
+    </div>
+  );
+}
+
+export default ExercisesList;
+
+
